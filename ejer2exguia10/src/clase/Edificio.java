f4784e8e7744e3ba0365bae1487c4c3000e4168b
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-09-28 21:44:49 -0300

    Add files via upload

---
+++ ejer2exguia10/src/clase/Edificio.java
@@ -0,0 +1,12 @@
+package clase;
+
+public abstract class Edificio {
+
+    public double ancho;
+    public double largo;
+    public double altura;
+
+    public abstract void calcularSuperficia();
+    public abstract void calcularVolumen();
+
+}
---
+++ ejer2exguia10/src/ejer2exguia10/Ejer2exguia10.java
@@ -0,0 +1,11 @@
+
+package ejer2exguia10;
+
+public class Ejer2exguia10 {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
